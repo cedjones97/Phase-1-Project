50816bfe67c62af88a47eccff5f07e563f693189
cedric <cedricj765@gmail.com>
2023-07-07 15:55:32 -0500

    fetch created

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 document.addEventListener('DOMContentLoaded', ()=>{
     const form = document.querySelector('form')
     form.addEventListener('submit', (e) =>{
+        
         wordSearch()
     })
 })
+
+function wordSearch(){
+    let input = document.querySelector('#search').value
+    //console.log(input)
+    fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${input}`)
+    .then(data => data.json())
+    .then(words => console.log(words))
+ }
