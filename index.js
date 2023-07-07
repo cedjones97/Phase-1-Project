87d37fc85260ae32831a2b15b93f0917b843b83b
cedric <cedricj765@gmail.com>
2023-07-07 16:31:50 -0500

    mouseover evenetListener created

--- index.js
+++ index.js
@@ -43,4 +43,7 @@ function newBtn(_word){
      def.innerText = _word.meanings[0].definitions[0].definition
      let hoverBtn = document.createElement('audio')
      hoverBtn.src = _word.phonetics[0].audio
+     hoverBtn.addEventListener('mouseover', ()=>{
+        audio.play()
+     })
 }
